b1c71efa4a6e51b742aaa555597108708f494b75
Hoang Nguyen <you@example.com>
2025-03-05 10:11:44 +0700

    add

--- ss4/src/Bt10.java
+++ ss4/src/Bt10.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Bt10 {
+public class NumberOperations {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         while (true) {
@@ -20,53 +20,47 @@ public class Bt10 {
             System.out.print("Nhập một số nguyên dương: ");
             int n = sc.nextInt();
 
-            switch (choice) {
-                case 1:
-                    if (isPrime(n)) {
-                        System.out.println(n + " là số nguyên tố.");
-                    } else {
-                        System.out.println(n + " không phải là số nguyên tố.");
+            if (choice == 1) {
+                boolean isPrime = true;
+                if (n < 2) {
+                    isPrime = false;
+                } else {
+                    for (int i = 2; i * i <= n; i++) {
+                        if (n % i == 0) {
+                            isPrime = false;
+                            break;
+                        }
                     }
-                    break;
-                case 2:
-                    if (isPerfect(n)) {
-                        System.out.println(n + " là số hoàn hảo.");
-                    } else {
-                        System.out.println(n + " không phải là số hoàn hảo.");
+                }
+                if (isPrime) {
+                    System.out.println(n + " là số nguyên tố.");
+                } else {
+                    System.out.println(n + " không phải là số nguyên tố.");
+                }
+            } else if (choice == 2) {
+                int sum = 0;
+                for (int i = 1; i < n; i++) {
+                    if (n % i == 0) {
+                        sum += i;
                     }
-                    break;
-                case 3:
-                    int sumDivisors = sumOfDivisors(n);
-                    System.out.println("Tổng các ước của " + n + " là: " + sumDivisors);
-                    break;
-                default:
-                    System.out.println("Lựa chọn không hợp lệ!");
+                }
+                if (sum == n) {
+                    System.out.println(n + " là số hoàn hảo.");
+                } else {
+                    System.out.println(n + " không phải là số hoàn hảo.");
+                }
+            } else if (choice == 3) {
+                int sum = 0;
+                for (int i = 1; i <= n; i++) {
+                    if (n % i == 0) {
+                        sum += i;
+                    }
+                }
+                System.out.println("Tổng các ước của " + n + " là: " + sum);
+            } else {
+                System.out.println("Lựa chọn không hợp lệ!");
             }
         }
         sc.close();
-    }
-
-    public static boolean isPrime(int n) {
-        if (n < 2) return false;
-        for (int i = 2; i * i <= n; i++) {
-            if (n % i == 0) return false;
-        }
-        return true;
-    }
-
-    public static boolean isPerfect(int n) {
-        int sum = 0;
-        for (int i = 1; i < n; i++) {
-            if (n % i == 0) sum += i;
-        }
-        return sum == n;
-    }
-
-    public static int sumOfDivisors(int n) {
-        int sum = 0;
-        for (int i = 1; i <= n; i++) {
-            if (n % i == 0) sum += i;
-        }
-        return sum;
     }
 }
